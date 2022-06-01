377c7bc99238063b8e6ab481b047d6218ad411e9
vaniseth <74897823+vaniseth@users.noreply.github.com>
2022-06-01 20:24:16 +0530

    GetPid

---
+++ SYSTEM CALLS/getpid.c
@@ -0,0 +1,18 @@
+
+#include<stdio.h>
+#include<unistd.h>
+int main(){
+    int pid,pid1,pid2; pid=fork(); 
+    if(pid==-1){
+    printf("ERROR IN PROCESS CREATION \n");
+    exit(1);
+    }
+    if(pid!=0){
+        pid1=getpid();
+        printf("\n the parent process ID is %d\n", pid1);
+    }
+    else{
+    pid2=getpid();
+    printf("\n the child process ID is %d\n", pid2);
+    }
+}
